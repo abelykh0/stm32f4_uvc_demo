020c8ab27bd5bdef3d62dfdd045f9289746f5149
Andrey Belykh <abelykh@yahoo.ca>
2024-11-10 20:49:43 -0500

    Using latest USB Video code

--- USB_DEVICE/Target/usbd_conf.c
+++ USB_DEVICE/Target/usbd_conf.c
@@ -364,12 +364,12 @@ USBD_StatusTypeDef USBD_LL_Init(USBD_HandleTypeDef *pdev)
   HAL_PCD_RegisterIsoOutIncpltCallback(&hpcd_USB_OTG_FS, PCD_ISOOUTIncompleteCallback);
   HAL_PCD_RegisterIsoInIncpltCallback(&hpcd_USB_OTG_FS, PCD_ISOINIncompleteCallback);
 #endif /* USE_HAL_PCD_REGISTER_CALLBACKS */
-//  HAL_PCDEx_SetRxFiFo(&hpcd_USB_OTG_FS, 0x80);    // 128
-//  HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 0, 0x40); // 64
-//  HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 1, 0x80); // 128
-    HAL_PCDEx_SetRxFiFo(&hpcd_USB_OTG_FS, 64);
-    HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 0, 16);
-    HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 1, 232);
+  HAL_PCDEx_SetRxFiFo(&hpcd_USB_OTG_FS, 0x80);    // 128
+  HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 0, 0x40); // 64
+  HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 1, 0x80); // 128
+    //HAL_PCDEx_SetRxFiFo(&hpcd_USB_OTG_FS, 64);
+    //HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 0, 16);
+    //HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_FS, 1, 232);
   }
   return USBD_OK;
 }
